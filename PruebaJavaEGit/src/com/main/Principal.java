47289fbb4648dbfd3efc899d8d5651bf6373b8a7
jorge <usuario@10.11.4.63>
2015-06-15 10:37:29 -0500

    Se sube clase principal con COMMIT

---
+++ PruebaJavaEGit/src/com/main/Principal.java
@@ -0,0 +1,14 @@
+package com.main;
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("iniciando");
+		
+		System.out.println("Finalizando...");
+	}
+
+}
