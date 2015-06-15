deb435202924ccd640898b447d6503a9f791d593
jorge <usuario@10.11.4.63>
2015-06-15 10:53:14 -0500

    Se sube cambio descargado de remoto a local

--- PruebaJavaEGit/src/com/main/Principal.java
+++ PruebaJavaEGit/src/com/main/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("iniciando");
 		System.out.println("Paso 1");
+		System.out.println("Paso 2");
 		System.out.println("Finalizando...");
 	}
 
