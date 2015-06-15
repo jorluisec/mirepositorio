a82ebcc2719806c1fffbf1bfa0bac31bf1b80a55
jorge <usuario@10.11.4.63>
2015-06-15 12:01:21 -0500

    Subo mis fuentes

--- PruebaJavaEGit2/src/com/main/Principal.java
+++ PruebaJavaEGit2/src/com/main/Principal.java
@@ -7,6 +7,7 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		System.out.println("iniciando");
+		System.out.println("Paso A");
 		System.out.println("Finalizando...");
 	}
 
