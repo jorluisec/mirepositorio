019f62105491d8924010433584ee8338d36bbf1f
jorge <usuario@10.11.4.63>
2015-06-15 12:04:13 -0500

    sube paso B

--- PruebaJavaEGit2/src/com/main/Principal.java
+++ PruebaJavaEGit2/src/com/main/Principal.java
@@ -8,6 +8,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("iniciando");
 		System.out.println("Paso A");
+		System.out.println("Paso B");
 		System.out.println("Finalizando...");
 	}
 
