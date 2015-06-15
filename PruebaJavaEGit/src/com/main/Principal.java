4ce5bf28779aaf2f9369161cbd5990b1707b3bfd
jorge <usuario@10.11.4.63>
2015-06-15 10:56:41 -0500

    Se agrega paso 3 con COMMIT

--- PruebaJavaEGit/src/com/main/Principal.java
+++ PruebaJavaEGit/src/com/main/Principal.java
@@ -9,6 +9,7 @@ public class Principal {
 		System.out.println("iniciando");
 		System.out.println("Paso 1");
 		System.out.println("Paso 2");
+		System.out.println("Paso 3");
 		System.out.println("Finalizando...");
 	}
 
