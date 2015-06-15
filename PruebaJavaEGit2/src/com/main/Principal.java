4764b84bf475e912e9cf01685674d0dadd34743c
jorge <usuario@10.11.4.63>
2015-06-15 11:54:15 -0500

    Se agrega fuentes

---
+++ PruebaJavaEGit2/src/com/main/Principal.java
@@ -0,0 +1,13 @@
+package com.main;
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("iniciando");
+		System.out.println("Finalizando...");
+	}
+
+}
